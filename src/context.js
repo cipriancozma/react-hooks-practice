1f6b16158ad5b488b2991e05aed1b608e394d73b
Ciprian <cozma_ciprian19@yahoo.com>
2020-11-24 08:57:06 +0200

    deleted info from context

--- src/context.js
+++ src/context.js
@@ -2,15 +2,15 @@ import React, { createContext } from 'react';
 
 const TodosContext = createContext({
     todos:[
-        {
-            id: 1, text: "Eat breakfast", completed: true
-        },
-        {
-            id: 2, text: "Eat lunch", completed: false
-        },
-        {
-            id: 3, text: "Eat dinner", completed: false
-        }
+        // {
+        //     id: 1, text: "Eat breakfast", completed: true
+        // },
+        // {
+        //     id: 2, text: "Eat lunch", completed: false
+        // },
+        // {
+        //     id: 3, text: "Eat dinner", completed: false
+        // }
     ],
     currentTodo: {}
 })
